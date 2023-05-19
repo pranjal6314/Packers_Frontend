5750db58c91d9107b033bf81c452feba395325dd
Pranjal Choudhary <77271332+pranjal6314@users.noreply.github.com>
2023-05-19 12:35:56 +0530

    Update index.js (#2)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import Head from "next/head";
-// import Link from "next/link";
+import Head from "next/head"; import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
